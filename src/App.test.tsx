02c4e2a6beb0aef49b44af1a4351540593eba412
nickphury <alisphere.c@gmail.com>
2022-03-13 20:43:12 +0100

    add config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Plug with nest api link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Plug with nest api');
   expect(linkElement).toBeInTheDocument();
+  debugger;
 });
